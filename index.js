700950f3576d65e227ec176eb337530757a898cb
JUDITHQM <jhqm.98@gmail.com>
2022-10-24 19:54:12 -0500

    añadiendo funciones puras para validar si la ruta existe y si es absoluta

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
-};
+//importando de file system 
+const fs = require('node:fs');
+//importando el path
+const path = require('node:path');
+
+//Funcion para ver si existe mi ruta
+const existpath = (route)=>{
+ return fs.existsSync(route)
+}
+console.log(existpath('C:/Users/jhqm_/OneDrive/Documentos/GitHub/LIM018-md-links/test/md-links.spec.js'));
+console.log(existpath('C:/Users/jhqm_/OneDrive/Documentos/GitHub/LIM018-md-links/test/md-links.js'));
+
+//Funcion para verificar si la ruta es absoluta
+const pathIsAbs = (route)=>{
+  return path.isAbsolute(route)
+}
+console.log("Aqui empieza la funcion para verificar si es una ruta absoluta")
+console.log(pathIsAbs('C:/Users/jhqm_/OneDrive/Documentos/GitHub/LIM018-md-links/test/md-links.spec.js'))
+console.log(pathIsAbs('md-links.spec.js'))
